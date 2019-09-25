50727ab1e9cc481bad61eb67e69a8a1e8537d179
kevbotliu <kevinliu732@gmail.com>
2019-09-25 15:49:37 -0700

    Add icon and badge with tab count

--- background.js
+++ background.js
@@ -1,13 +1,24 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  
-});
+chrome.tabs.onCreated.addListener(tab => updateBadgeTabCount());
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => updateBadgeTabCount());
+chrome.tabs.onDetached.addListener((tabId, detachInfo) => updateBadgeTabCount());
+chrome.tabs.onAttached.addListener((tabId, attachInfo) => updateBadgeTabCount());
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => updateBadgeTabCount());
+chrome.windows.onFocusChanged.addListener(windowId => updateBadgeTabCount());
+
+function updateBadgeTabCount() {
+  chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+  chrome.tabs.query({currentWindow: true}, tabs => {
+    chrome.browserAction.setBadgeText({text: tabs.length.toString()});
+  });
+}
+
+
 
 
 chrome.runtime.onStartup.addListener(() => {
 
 })
 
-
-chrome.storage.onChanged.addListener((changes, areaName) => {
-  console.log(changes);
-})
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('Command:', command);
+});
