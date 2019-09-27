8b56e515d429f6d9b0dd48e5aa0e03af4dd53434
kevbotliu <kevinliu732@gmail.com>
2019-09-26 18:58:47 -0700

    Add initial options.html

--- popup.js
+++ popup.js
@@ -217,4 +217,14 @@ function submitInput(name) {
   }); 
 }
 
+document.getElementById('options-link').addEventListener('click', () => {
+  var optionsUrl = chrome.extension.getURL('options.html');
 
+  chrome.tabs.query({url: optionsUrl}, function(tabs) {
+      if (tabs.length) {
+          chrome.tabs.update(tabs[0].id, {active: true});
+      } else {
+          chrome.tabs.create({url: optionsUrl});
+      }
+  });
+})
